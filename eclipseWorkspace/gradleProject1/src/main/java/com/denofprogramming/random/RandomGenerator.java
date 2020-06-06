7f3287971d4be2603dd848527b7d317e0c5b65b8
pypalkar23 <mandar.palkar94@gmail.com>
2020-06-06 17:37:12 +0530

    Adding two new projects made for gradle tutorial

---
+++ eclipseWorkspace/gradleProject1/src/main/java/com/denofprogramming/random/RandomGenerator.java
@@ -0,0 +1,6 @@
+package com.denofprogramming.random;
+
+public interface RandomGenerator {
+	String name();
+	int generate();
+}
---
+++ eclipseWorkspace/gradleProjectWeb/src/main/java/com/denofprogramming/random/RandomGenerator.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package com.denofprogramming.random;
+
+
+/**
+ * @author Mandar
+ *
+ */
+public interface RandomGenerator
+{
+
+    String name();
+
+    GeneratedRandomIdentifier generate();
+
+}
